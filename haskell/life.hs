c3d1c1a4528f9de2b4edd08861a4f12c6a032ce8
Eric Newton <eric@area1security.com>
2021-12-07 12:46:04 -0500

    define types for everything

--- haskell/life.hs
+++ haskell/life.hs
@@ -6,7 +6,12 @@ import Debug.Trace
 
 humanSpeed = 1 / 30.0
 generations = 1000
+data State = Alive | Dead
 data DestinyType = Live | Die | Ignore deriving (Show, Eq, Ord)
+data Coord = Coord {coord_x :: Int, coord_y :: Int } deriving (Show, Eq, Ord)
+type LiveSet = Set.Set(Coord)
+type Change = (DestinyType, Coord)
+type Board = (LiveSet, [Change])
 
 clearScreen :: IO ()
 clearScreen = do
@@ -17,24 +22,23 @@ clearScreen = do
   where
     esc = chr(27)
 
-cell :: (Ord x, Ord y) => Set.Set (x, y) -> x -> y -> Char
-cell liveSet x y =
-  if (Set.member (x, y) liveSet) then '@' else ' '
+cell :: LiveSet -> Coord -> Char
+cell liveSet coord =
+  if (Set.member coord liveSet) then '@' else ' '
 
-row :: (Ord x, Enum x, Ord y) =>
-     Set.Set (x, y) -> y -> x -> x -> [Char]
+row :: LiveSet -> Int -> Int -> Int -> [Char]
 row liveSet y minx maxx =
-  map (\x -> cell liveSet x y) [minx .. maxx]
+  map (\x -> cell liveSet (Coord x y)) [minx .. maxx]
 
 
-expandBox :: (Ord a, Ord b) => ((a, b), (a, b)) -> (a, b) -> ((a, b), (a, b))
+expandBox :: ((Int, Int), (Int, Int)) -> Coord -> ((Int, Int), (Int, Int))
 expandBox box point =
   ((min minx x, min miny y), (max maxx x, max maxy y))
   where
     ((minx, miny), (maxx, maxy)) = box
-    (x, y) = point
+    (x, y) = (coord_x point, coord_y point)
 
-bbox :: Foldable t => t (Int, Int) -> ((Int, Int), (Int, Int))
+bbox :: Foldable t => t (Coord) -> ((Int, Int), (Int, Int))
 bbox lst =
   foldl expandBox tinybox lst
   where
@@ -54,40 +58,47 @@ printBoard board = do
   putStrLn (asString board)
   threadDelay (round (1000000 * humanSpeed))
 
+applyUpdates :: LiveSet -> [Change] -> LiveSet
 applyUpdates liveSet updates =
   Set.union (Set.difference liveSet toDie) toLive
   where
     toLive = Set.fromList [pos | (Live, pos) <- updates]
     toDie = Set.fromList [pos | (Die, pos) <- updates]
 
+neighbors :: Coord -> [Coord]
 neighbors pos =
-  [(posx + x, posy + y)| x <- [-1 .. 1], y <- [-1 .. 1], x /= 0 || y /= 0]
+  [Coord (posx + x) (posy + y)| x <- [-1 .. 1], y <- [-1 .. 1], x /= 0 || y /= 0]
   where
-    (posx, posy) = pos
+    (posx, posy) = (coord_x pos, coord_y pos)
 
+considerSet :: [Coord] -> Set.Set(Coord)
 considerSet positions =
   Set.fromList (concat (map neighbors positions))
 
 -- rules of life
+change :: Int -> State -> DestinyType
 change 2 _     = Ignore
-change 3 False = Live
-change 3 True  = Ignore
-change _ True  = Die
+change 3 Dead  = Live
+change 3 Alive = Ignore
+change _ Alive = Die
 change _ _     = Ignore
 
+computeChange :: LiveSet -> Coord -> Change
 computeChange liveSet pos =
   (how, pos)
   where
     neighborCount = length [True|n <- (neighbors pos), Set.member n liveSet]
-    isAlive = Set.member pos liveSet
-    how = change neighborCount isAlive
+    state = if Set.member pos liveSet then Alive else Dead
+    how = change neighborCount state
 
+computeUpdates :: LiveSet -> [Change] -> [Change]
 computeUpdates liveSet updates =
   [(how, pos)|(how, pos) <- (Set.toList newUpdates), how /= Ignore]
   where
     changed = [pos|(how, pos) <- updates]
     newUpdates = Set.map (\ pos -> computeChange liveSet pos) (considerSet changed)
 
+next :: Board -> Board
 next board =
   (newSet, newUpdates)
   where
@@ -95,6 +106,7 @@ next board =
     newSet = applyUpdates liveSet updates
     newUpdates = computeUpdates newSet updates
 
+printGeneration :: Int -> Board -> IO ()
 printGeneration 0 board = do
   -- do something with the last board to force evaluation
   -- otherwise nothing gets computed and we can't benchmark
@@ -104,21 +116,20 @@ printGeneration 0 board = do
 
 printGeneration n board =
   do
+    -- for standard game-of-life display, uncomment this line:
     -- printBoard board
     printGeneration (n - 1) (next board)
 
+run :: Int -> IO ()
 run n = do
   printGeneration n start
   where
     r_pentomino = [(0, 0), (0, 1), (1, 1), (-1, 0), (0, -1)]
-    update = map (\ pos -> (Live, pos)) r_pentomino
-    -- empty Sets don't "show", so throw in a value to keep it
-    -- from being empty and we can get some debugging done
-    -- since this is the first value in the initial generation, it's
-    -- basically ignored
-    liveSet = (Set.fromList [head r_pentomino])
+    update = map (\ (x, y) -> (Live, Coord x y)) r_pentomino
+    liveSet = (Set.fromList [])
     start = (liveSet, update)
-    
+
+main :: IO ()
 main = do
   run generations
   run generations
